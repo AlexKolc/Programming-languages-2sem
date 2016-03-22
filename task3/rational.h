ad158aa45f056e2073484a91cccbbf4cf13127f5
AlexKolc <alex_kolc@inbox.ru>
2016-03-22 03:07:12 +0300

    Update rational.h

--- task3/rational.h
+++ task3/rational.h
@@ -1,21 +1,22 @@
 #ifndef TASK3_RATIONAL_H
 #define TASK3_RATIONAL_H
 
-class rational
-{
-    private:
-        int n;
-        int d;
-        int gcd(int, int);
-    public:
-        rational(int i);
-        rational(int num, int denum);
-        int getNum() const;
-        int getDenom() const;
-        rational operator +(rational const & x) const;
-        rational operator -(rational const & x) const;
-        rational operator *(rational const & x) const;
-        rational operator /(rational const & x) const;
+class rational {
+private:
+    int n;
+    int d;
+    int gcd(int a, int b) const;
+public:
+    rational(int num, int denom);
+    rational(int num);
+    int getNum() const;
+    int getDenom() const;
+    rational operator +(rational const &x) const;
+    rational operator -(rational const &x) const;
+    rational operator *(rational const &x) const;
+    rational operator /(rational const &x) const;
+
 };
 
-#endif
+
+#endif //TASK3_RATIONAL_H
