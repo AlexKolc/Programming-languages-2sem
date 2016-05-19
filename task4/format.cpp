09e7d9f937fba5ba62eb3030cf8ef2311b896bfe
AlexKolc <alex_kolc@inbox.ru>
2016-05-19 13:30:03 +0300

    First commit

---
+++ task4/format.cpp
@@ -0,0 +1,36 @@
+#include "format.h"
+
+using namespace std;
+
+namespace Format {
+    string check_specifier(const string &fmt, unsigned &pos, int flag) {
+        string outcome = "";
+        for (; pos < fmt.length();) {
+            while (pos < fmt.length() && fmt[pos] != '%') outcome += fmt[pos++];
+            if (pos == fmt.length() - 1)
+                throw invalid_argument("Format cann't be '%%'");
+            if (pos == fmt.length()) {
+                if (flag)
+                    throw invalid_argument("Format cann't have a number of arguments.(many)");
+                return outcome;
+            }
+            if (fmt[pos + 1] == '%')
+                outcome += '%', pos += 2;
+            else {
+                pos++;
+                if (!flag)
+                    throw out_of_range("Format cann't have a number of arguments.(few)");
+                break;
+            }
+        }
+        return outcome;
+    }
+
+    string format_impl(const string &fmt, unsigned pos, unsigned outprint) {
+        return check_specifier(fmt, pos, 0);
+    }
+
+    string nullptr_exception(nullptr_t force) {
+        return "nullptr";
+    }
+}
